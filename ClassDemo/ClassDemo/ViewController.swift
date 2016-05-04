a9fc82743dcc74c8cf112d9bd51a7de8c4f46d83
wanqingrongruo <devzheng@qq.com>
2016-05-04 10:01:42 +0800

    Swift:类的学习

---
+++ ClassDemo/ClassDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassDemo
+//
+//  Created by 婉卿容若 on 16/5/4.
+//  Copyright © 2016年 婉卿容若. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
